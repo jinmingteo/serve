4626e7c2bf7cef42068ed81bafab56ffbcb37292
dh <jinmingteo95@gmail.com>
2021-03-19 18:39:50 +0800

    [PREPROCESSING] edit image preprocessing compose

--- ts/torch_handler/image_classifier.py
+++ ts/torch_handler/image_classifier.py
@@ -19,8 +19,9 @@ class ImageClassifier(VisionHandler):
     # These are the standard Imagenet dimensions
     # and statistics
     image_processing = transforms.Compose([
-        transforms.Resize(256),
-        transforms.CenterCrop(224),
+        transforms.Resize(224, interpolation=transforms.InterpolationMode.LANCZOS),
+        #transforms.Resize(256),
+        #transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406],
                              std=[0.229, 0.224, 0.225])
